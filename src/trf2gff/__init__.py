5fe16c30a666f09bfd9fe9d7293c20d0d9c44f8e
Adam Taranto <adam.p.taranto@gmail.com>
2025-11-09 21:07:09 +1100

    Add docstrings

--- src/trf2gff/__init__.py
+++ src/trf2gff/__init__.py
@@ -1,1 +1,35 @@
+"""
+TRF2GFF: Tandem Repeat Finder to GFF3 Converter.
 
+A command-line tool for converting Tandem Repeat Finder (TRF) .dat file
+output into standardized GFF3 format for downstream genomic analysis.
+
+This package enables integration of tandem repeat annotations with genome
+browsers and bioinformatics tools that use the GFF3 standard.
+
+The main application module (app) contains CLI and conversion logic.
+
+Notes
+-----
+For more information on Tandem Repeat Finder, see:
+https://tandem.bu.edu/trf/trf.html
+
+For GFF3 format specification, see:
+https://github.com/The-Sequence-Ontology/Specifications/blob/master/gff3.md
+
+Examples
+--------
+Command-line usage:
+
+Convert TRF output to GFF3 file:
+
+    $ trf2gff -i genome.fa.2.7.7.80.10.50.500.dat -o repeats.gff3
+
+Process stdin and write to stdout:
+
+    $ cat input.dat | trf2gff -o -
+"""
+
+from trf2gff._version import __version__
+
+__all__ = ["__version__"]
--- src/trf2gff/app.py
+++ src/trf2gff/app.py
@@ -2,19 +2,17 @@
 # -*- coding: UTF-8 -*-
 
 """
-
-████████╗██████╗ ███████╗██████╗  ██████╗ ███████╗███████╗
-╚══██╔══╝██╔══██╗██╔════╝╚════██╗██╔════╝ ██╔════╝██╔════╝
-   ██║   ██████╔╝█████╗   █████╔╝██║  ███╗█████╗  █████╗  
-   ██║   ██╔══██╗██╔══╝  ██╔═══╝ ██║   ██║██╔══╝  ██╔══╝  
-   ██║   ██║  ██║██║     ███████╗╚██████╔╝██║     ██║     
-   ╚═╝   ╚═╝  ╚═╝╚═╝     ╚══════╝ ╚═════╝ ╚═╝     ╚═╝     
-
-Converts Tandem Repeat Finder .dat file output into GFF3 format                                                          
-
+Converts Tandem Repeat Finder .dat file output into GFF3 format.
 """
 
-# Copyright (C) 2017-2023 Adam Taranto <adam.p.taranto@gmail.com>
+# ████████╗██████╗ ███████╗██████╗  ██████╗ ███████╗███████╗
+# ╚══██╔══╝██╔══██╗██╔════╝╚════██╗██╔════╝ ██╔════╝██╔════╝
+#    ██║   ██████╔╝█████╗   █████╔╝██║  ███╗█████╗  █████╗
+#    ██║   ██╔══██╗██╔══╝  ██╔═══╝ ██║   ██║██╔══╝  ██╔══╝
+#    ██║   ██║  ██║██║     ███████╗╚██████╔╝██║     ██║
+#    ╚═╝   ╚═╝  ╚═╝╚═╝     ╚══════╝ ╚═════╝ ╚═╝     ╚═╝
+
+# Copyright (C) 2017-present Adam Taranto <adam.p.taranto@gmail.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -29,16 +27,63 @@ Converts Tandem Repeat Finder .dat file output into GFF3 format
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-
-from trf2gff._version import __version__
 import argparse
 import csv
 import logging
 import os.path as op
 import sys
 
+from trf2gff._version import __version__
+
 
 def mainArgs():
+    """
+    Parse command-line arguments for TRF to GFF3 conversion.
+
+    Configures argument parser for input/output file paths, version display,
+    and logging level settings. Automatically generates output filename from
+    input filename if not specified and stdin is not being used.
+
+    Returns
+    -------
+    argparse.Namespace
+        Parsed command-line arguments containing:
+
+        - infile : str or None
+            Path to input TRF .dat file
+        - outgff : str
+            Path to output GFF3 file or '-' for stdout
+        - loglevel : str
+            Logging level (DEBUG, INFO, WARNING, ERROR, CRITICAL)
+
+    See Also
+    --------
+    main : Main conversion function that uses these arguments.
+
+    Notes
+    -----
+    If no output file is specified:
+
+    - When reading from stdin: defaults to stdout ('-')
+    - When reading from file: generates filename as <input_base>.gff3
+
+    Examples
+    --------
+    Parse arguments for file input with auto-generated output name:
+
+    >>> import sys
+    >>> sys.argv = ['trf2gff', '-i', 'genome.dat']
+    >>> args = mainArgs()
+    >>> args.outgff
+    'genome.gff3'
+
+    Parse arguments for stdout output:
+
+    >>> sys.argv = ['trf2gff', '-i', 'genome.dat', '-o', '-']
+    >>> args = mainArgs()
+    >>> args.outgff
+    '-'
+    """
     parser = argparse.ArgumentParser(
         description="Converts Tandem Repeat Finder .dat file output into GFF3 format",
         prog="trf2gff",
@@ -81,6 +126,65 @@ def mainArgs():
 
 
 def main():
+    """
+    Convert Tandem Repeat Finder .dat output to GFF3 format.
+
+    Main entry point for the trf2gff command-line tool. Reads TRF .dat file
+    data from either stdin or a specified file, parses tandem repeat features,
+    and outputs them in GFF3 format to stdout or a specified file.
+
+    The function performs the following workflow:
+
+    1. Parse command-line arguments
+    2. Configure logging
+    3. Read input data (stdin or file)
+    4. Parse TRF dat format lines
+    5. Convert features to GFF3 format
+    6. Write output (stdout or file)
+
+    TRF .dat files contain sequence headers ("Sequence: <name>") followed by
+    data lines with 15 space-separated fields per tandem repeat feature.
+
+    GFF3 output includes columns for seqid, source (TRF), type (TRF_SSR),
+    start, end, score (.), strand (.), phase (.), and attributes with
+    key=value pairs containing TRF metadata.
+
+    Raises
+    ------
+    SystemExit
+        If input file doesn't exist or no input data is provided.
+    ValueError
+        If an invalid log level is specified.
+
+    See Also
+    --------
+    mainArgs : Parse command-line arguments.
+
+    Notes
+    -----
+    Input priority order:
+
+    1. stdin (highest priority)
+    2. --infile argument
+    3. Error if neither available
+
+    When reading from stdin, any --infile argument is ignored.
+
+    Examples
+    --------
+    Convert file to stdout:
+
+    >>> # Command line usage:
+    >>> # trf2gff -i genome.fa.2.7.7.80.10.50.500.dat -o -
+
+    Convert file with auto-generated output name:
+
+    >>> # trf2gff -i genome.fa.2.7.7.80.10.50.500.dat
+
+    Process stdin input:
+
+    >>> # cat input.dat | trf2gff -o output.gff3
+    """
     # Fetch args
     args = mainArgs()
 
@@ -96,7 +200,7 @@ def main():
 
     if not sys.stdin.isatty():
         # If data on stdin, set input_data to read from stdin
-        logging.info(f"Reading input from stdin.")
+        logging.info("Reading input from stdin.")
         input_data = sys.stdin.readlines()
         if args.infile:
             # If infile is also set log that it will be ignored
@@ -115,7 +219,7 @@ def main():
                 input_data = fh.readlines()
     else:
         # Exit if no data on stdin or infile
-        logging.error(f"No input data detected. Exiting.")
+        logging.error("No input data detected. Exiting.")
         sys.exit(1)
 
     # Init feature counter
@@ -208,7 +312,7 @@ def main():
             # Convert list items to csv lines and write.
             csv_writer.writerows(outlines)
     else:
-        logging.info(f"Writing gff to stdout")
+        logging.info("Writing gff to stdout")
         # Create csv writer with tab delimiters.
         csv_writer = csv.writer(sys.stdout, delimiter="\t")
         # Convert list items to csv lines and write.
